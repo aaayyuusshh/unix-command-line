cc5caf32b81a31552c8edc4370df3f59c6355eec
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-02-05 22:48:42 -0700

    Got user's command line input

--- commandLine.cpp
+++ commandLine.cpp
@@ -4,8 +4,22 @@
 #include <string.h>
 #include <fcntl.h>
 
+using namespace std;
+
 int main(){
 
+   //getting the user's command line input
+   string userInput;
+   char buffer [100];
+
+   printf("Input command line: ");
+   fgets(buffer, 100, stdin);
+   buffer[strcspn(buffer, "\n")] = 0;
+
+   printf("User Input: %s", buffer);
+   printf("hello\n");
+
+   system(buffer);
    
    return 0;
-}
+} 
