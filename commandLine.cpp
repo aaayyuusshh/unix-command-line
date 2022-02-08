56eabef51d1eefd967d92ed23e218d47a77313d7
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-02-08 00:12:32 -0700

    Using int value from parser to divide cases now

--- commandLine.cpp
+++ commandLine.cpp
@@ -35,24 +35,35 @@ int main(){
    printf("\n");
    //system(buffer);
 
-   /* --CASE 1: execute a single cmd line w/ upto one arg-- */
+    int cases= parser(buffer);
 
-   char *token;
-   token = strtok(buffer, " "); //getting every individual word from user input split by delimeter " "
-   int idx= 0;
-   char *args[3];  //char *args [] = {"ls", "-l", NULL};
+    /* -- CASE 1: execute a single cmd line w/ upto one arg -- */
+    if(cases == 1){
+        printf("CASE 1\n");
 
-   while(token != NULL){
-       printf("Token %d:%s\n", idx, token);
-       args[idx] = token;  //populating args with individual words from user input split by " "
-       idx++;
-       token = strtok(NULL, " ");   
-   }
-   args[2] = NULL;
-   printf("\n");
- 
-   //if user input = "ls -l" then buffer = "ls -l" then args = ["ls", "-l", NULL] 
-   execvp(args[0], args);  
+        char *token;
+        token = strtok(buffer, " "); //getting every individual word from user input split by delimeter " "
+        int idx= 0;
+        char *args[3];  //char *args [] = {"ls", "-l", NULL};
+
+        while(token != NULL){
+            printf("Token %d:%s\n", idx, token);
+            args[idx] = token;  //populating args with individual words from user input split by " "
+            idx++;
+            token = strtok(NULL, " ");   
+        }
+        args[2] = NULL;
+        printf("\n");
+        
+        //if user input = "ls -l" then buffer = "ls -l" then args = ["ls", "-l", NULL] 
+        execvp(args[0], args); 
+    }  
+
+    /* -- CASE 2: Execute two commands connected w/ a pipe & redirection -- */
+    else if(cases == 2){
+        printf("CASE 2\n");
+
+    }
 
    return 0;
 } 
