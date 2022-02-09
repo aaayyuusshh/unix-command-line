78d696cf41d12f692aa45615ef0bae4ebabe44c9
aaayyuusshh <80851741+aaayyuusshh@users.noreply.github.com>
2022-02-09 02:22:03 -0700

    case 1 & case 2 both use system() + infinite while loop for user input

--- commandLine.cpp
+++ commandLine.cpp
@@ -25,122 +25,81 @@ int parser(char aString[]){
 
 int main(){
 
-   /* getting the user's command line input from stdin & storing it into buffer */
-   char buffer [50];
-   printf("Input command line: ");
-   fgets(buffer, 50, stdin);
-   buffer[strcspn(buffer, "\n")] = 0; //removes the \n character from buffer 
+    while(1){
 
-   printf("Buffer: %s\n", buffer);
-   printf("\n");
-   //system(buffer);
+        /* getting the user's command line input from stdin & storing it into buffer */
+        char buffer [50];
+        printf("Input command line: ");
+        fgets(buffer, 50, stdin);
+        buffer[strcspn(buffer, "\n")] = 0; //removes the \n character from buffer 
 
-    int cases= parser(buffer);
-
-    /* -- CASE 1: execute a single cmd line w/ upto one arg -- */
-    if(cases == 1){
-        printf("CASE 1\n");
-
-        char *token;
-        token = strtok(buffer, " "); //getting every individual word from user input split by delimeter " "
-        int idx= 0;
-        char *args[3];  //char *args [] = {"ls", "-l", NULL};
-
-        while(token != NULL){
-            printf("Token %d:%s\n", idx, token);
-            args[idx] = token;  //populating args with individual words from user input split by " "
-            idx++;
-            token = strtok(NULL, " ");   
-        }
-        args[2] = NULL;
+        printf("Buffer: %s\n", buffer);
         printf("\n");
-        
-        //if user input = "ls -l" then buffer = "ls -l" then args = ["ls", "-l", NULL] 
-        execvp(args[0], args); 
-    }  
+        //system(buffer);
 
-    /* -- CASE 2: Execute two commands connected w/ a pipe & redirection -- */
-    else if(cases == 2){
-        printf("CASE 2\n");
+        int cases= parser(buffer);
 
-        system(buffer);
+        /* -- CASE 1: execute a single cmd line w/ upto one arg -- */
+        /* -- CASE 2: Execute two commands connected w/ a pipe & redirection -- */
+
+        if(cases == 1 || cases == 2){
+            printf("CASE 2\n");
+
+            system(buffer);
+        }
+
+        /* -- CASE 3: Special Pipes -- */
+
+        // "cmd1 cmd2 $ cmd3 cmd4"
+        else if(cases == 3){
+
+            //STRING PARSING WORK : putting input(buffer) into inputArr
+            char *token;
+            token= strtok(buffer, " ");
+            char *inputArr[20];
+            int idx=0; //also servers as the length of the array
+            while(token!= NULL){
+                inputArr[idx] = token;
+                idx++;
+                token= strtok(NULL, " ");
+            }
+
+            //printing each element of inputArr and finding index of "$"
+            int dollar;
+            for(int i=0; i< idx; i++){
+                printf("%d:%s\n",i, inputArr[i]);
+                if(strncmp(inputArr[i],"$", 1) == 0){
+                    dollar = i;
+                }    
+            }
+
+            //    0     1     2    3     4
+            // [ "ls" "pwd"  "$" "wc" "cat" ]
+
+            int fd[2];
+            pipe(fd);
+    
+            //process everything before the "$" and add the cmds output into fd[1]
+            for(int i =0; i< dollar; i++){
+
+                char *args[]= {inputArr[i], NULL};
+
+                pid_t pid = fork(); //both ends of pipe are open
+                if(pid == 0){ //child --> used to write into pipe
+                    close(fd[0]);
+                    dup2(fd[1],1);
+                    close(fd[1]);
+                    execvp(inputArr[i], args); 
+                }
+
+                else{
+                    wait(NULL);
+                }
+            }
+        }
     }
 
-    /* -- CASE 3: Special Pipes -- */
-
-    // "cmd1 cmd2 $ cmd3 cmd4"
-    else if(cases == 3){
-
-        //STRING PARSING WORK : putting input(buffer) into inputArr
-        char *token;
-        token= strtok(buffer, " ");
-        char *inputArr[20];
-        int idx=0; //also servers as the length of the array
-        while(token!= NULL){
-            inputArr[idx] = token;
-            idx++;
-            token= strtok(NULL, " ");
-        }
-
-        //printing each element of inputArr and finding index of "$"
-        int dollar;
-        for(int i=0; i< idx; i++){
-            printf("%d:%s\n",i, inputArr[i]);
-            if(strncmp(inputArr[i],"$", 1) == 0){
-                dollar = i;
-            }    
-        }
-
-        // ls $ wc wc
-        int fd[2];
-        pipe(fd);
-
-        //process everything before the "$" and add the cmds output into fd[1]
-        for(int i=0; i<dollar; i++){ 
-
-            char *args[] = {inputArr[i], NULL};
-            pid_t pid= fork();
-
-            if(pid == 0){ //child --> writes stdout(1) to the write end of the pip (fd[1])
-                close(fd[0]);
-                dup2(fd[1], 1); //put whats in stdout into fd[1]
-                close(fd[1]);
-                execvp(inputArr[i], args);
-            }
-
-            else{
-                //wait(NULL);
-            }
-        }
-
-         //ls $ wc wc 
-        //process right of $ with stdin redireciton (inputs from read end of pipe)
-        for(int i= dollar+1; i< idx; i++){ 
-            char *args[] = {inputArr[i], NULL};
-            pid_t pid= fork();
-
-            if(pid == 0){ 
-                close(fd[1]);
-                dup2(fd[0], 0);
-                close(fd[0]);
-                execvp(inputArr[i], args);
-            }
-
-            else{
-                //wait(NULL);
-            }
-        }
-
-        // close(fd[1]);
-        // FILE *readFile = fdopen(fd[0], "r");
-        // char toRead= fgetc(readFile);
-        // while(toRead != EOF){
-        //     printf("%c", toRead);
-        //     //printf("!");
-        //     toRead= fgetc(readFile);
-        // }
-
-    }
+    
 
    return 0;
 } 
